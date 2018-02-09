c32163b202e9be90442b428befab82bc742988d7
D Vargas <hiphipvargas@sapo.pt>
2018-02-09 12:46:27 +0100

    OPENEUROPA-208: Add theme files to conventions.

--- test/DrupalTest.php
+++ test/DrupalTest.php
@@ -7,9 +7,9 @@ use GrumPHP\Collection\FilesCollection;
 use GrumPHP\Task\Context\GitPreCommitContext;
 
 /**
- * Tests for git commit message conventions.
+ * Tests for Drupal conventions.
  */
-class PhpMessDetectorTest extends AbstractTest
+class DrupalTest extends AbstractTest
 {
     /**
      * Tests different git messages against the predefined conventions.
@@ -23,7 +23,7 @@ class PhpMessDetectorTest extends AbstractTest
      */
     public function testPhpCodeMessage($fixture, $expected)
     {
-        $container = $this->getContainer($this->getDistPath().'/base-conventions.yml');
+        $container = $this->getContainer($this->getDistPath().'/drupal-conventions.yml');
         $collection = new FilesCollection([$this->getFixture($fixture)]);
         $context = new GitPreCommitContext($collection);
         /** @var \GrumPHP\Task\TaskInterface $task */
@@ -35,14 +35,18 @@ class PhpMessDetectorTest extends AbstractTest
     /**
      * Test case provider function.
      *
+     * Test file extensions.
+     *
      * @return array
      *      Test data.
      */
     public function commitMessageProvider()
     {
         return [
-          ['correct-code.php', TaskResult::PASSED],
-          ['incorrect-code.php', TaskResult::FAILED],
+          ['correct-code.inc', TaskResult::PASSED],
+          ['correct-code.module', TaskResult::PASSED],
+          ['correct-code.theme', TaskResult::PASSED],
+          ['ignored-code.xxx', TaskResult::SKIPPED],
         ];
     }
 }
