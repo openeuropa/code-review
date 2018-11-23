65cdb40568327030f0799d77ed4ff4093558ca52
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-11-23 14:12:04 +0100

    Issue #45: Fix errors when the tasks key is not an array or empty.

--- src/ExtraTasksExtension.php
+++ src/ExtraTasksExtension.php
@@ -4,7 +4,6 @@ namespace OpenEuropa\CodeReview;
 
 use GrumPHP\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\RuntimeException;
 
 /**
  * Extension that allows to define extra tasks on local grumphp.yml.dist.
@@ -16,15 +15,19 @@ class ExtraTasksExtension implements ExtensionInterface
      */
     public function load(ContainerBuilder $container)
     {
-        if ($container->hasParameter('extra_tasks')) {
-            $tasks = $container->getParameter('tasks');
-            foreach ($container->getParameter('extra_tasks') as $name => $value) {
-                if (array_key_exists($name, $tasks)) {
-                    throw new RuntimeException("Cannot override already defined task '{$name}' in 'extra_tasks'.");
-                }
-                $tasks[$name] = $value;
-            }
-            $container->setParameter('tasks', $tasks);
+        if (!$container->hasParameter('extra_tasks')) {
+            return;
         }
+        $tasks = (array) $container->getParameter('tasks');
+
+        foreach ($container->getParameter('extra_tasks') as $name => $value) {
+            if (\array_key_exists($name, $tasks)) {
+                throw new \RuntimeException("Cannot override already defined task '{$name}' in 'extra_tasks'.");
+            }
+
+            $tasks[$name] = $value;
+        }
+
+        $container->setParameter('tasks', $tasks);
     }
 }
