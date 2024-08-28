87b057fbaabe984d38160c132201f2e25d7b688d
AaronGilMartinez <aaron.gil-martinez@ext.ec.europa.eu>
2024-08-28 13:14:46 +0200

    OEL-3327: Cache variable.

--- tests/src/AbstractTest.php
+++ tests/src/AbstractTest.php
@@ -31,16 +31,17 @@ abstract class AbstractTest extends TestCase
     {
         // Create a GrumPHP configuration file to use in the test. Check if a configuration specific file exists, or
         // fall back to a generic template file.
-        $filename = file_exists(realpath(__DIR__ . "/../config/$configuration.yml.dist"))
-        ?  realpath(__DIR__ . "/../config/$configuration.yml.dist")
-        :  realpath(__DIR__ . '/../config/grumphp.yml.dist');
+        $realpath = realpath(__DIR__ . '/..');
+        $filename = file_exists("$realpath/config/$configuration.yml.dist")
+        ? "$realpath/config/$configuration.yml.dist"
+        : "$realpath/config/grumphp.yml.dist";
         $content = file_get_contents($filename);
         $content = str_replace("{configuration}", $configuration, $content);
-        file_put_contents(realpath(__DIR__ . '/../config/grumphp.yml'), $content);
+        file_put_contents("$realpath/config/grumphp.yml", $content);
 
         // Initialise the application with the provided config.
         $input = new ArrayInput([
-            '--config' => realpath(__DIR__ . '/../config/grumphp.yml'),
+            '--config' => "$realpath/config/grumphp.yml",
         ]);
         // Mark the application as non-interactive, so turn off any request for input during task execution.
         $input->setInteractive(false);
@@ -103,7 +104,7 @@ abstract class AbstractTest extends TestCase
      */
     public function getFixture($fixture)
     {
-        $file = new \SplFileInfo(realpath(__DIR__ . '/../fixtures/' . $fixture));
+        $file = new \SplFileInfo(realpath(__DIR__ . '/..') . '/fixtures/' . $fixture);
         if (!$file->isReadable()) {
             throw new \RuntimeException(sprintf('The fixture %s could not be loaded!', $fixture));
         }
